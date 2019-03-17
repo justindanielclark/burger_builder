0e670ff964671815b1c867b63f3fe8b13f887847
justindanielclark@gmail.com <justindanielclark@gmail.com>
2019-03-17 15:58:08 -0700

    Minor Change - Testing Git

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Making Changes
           </a>
         </header>
       </div>
